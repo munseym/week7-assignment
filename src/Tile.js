5ffd04ae9b26011b333ff067c292985b9f5609c0
Michael Munsey <michael.r.munsey@boeing.com>
2019-06-05 16:00:00 -0700

    Display questions when tile is clicked

--- src/Tile.js
+++ src/Tile.js
@@ -4,25 +4,49 @@ class Tile extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentState: 'dollars' 
+            currentState: 'dollars'
         };
+
+        this.answers = this.props.incorrect_answers;
+        this.answers.push(this.props.correct_answer);
+    }
+
+    handleClick = (e) => {
+        if (this.state.currentState === 'dollars') {
+            this.setState({
+                currentState: 'answerQuestion'
+            });
+        }
+    };
+
+    shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+            [array[i], array[j]] = [array[j], array[i]]; // swap elements
+        }
     }
 
     render() {
-        let val = this.props.id;
-        val += 1;
-        val *= 100;
-        
-        if(this.state.currentState === 'dollars'){
+        const val = (this.props.id + 1) * 100;
+        this.shuffle(this.answers);
+
+        if (this.state.currentState === 'dollars') {
             return (
-                <div className="card">
-                    {val}
-                </div>
+                <a href onClick={this.handleClick}>
+                    <div className="card">
+                        {val}
+                    </div>
+                </a>
             )
         }
         else return (
             <div>
                 <div className="card" dangerouslySetInnerHTML={{ __html: this.props.question }} />
+                <div>
+                    <ul>
+                        {this.answers.map(incorrectAnswer => <li>{incorrectAnswer}</li>)}
+                    </ul>
+                </div>
             </div>
         );
     }
